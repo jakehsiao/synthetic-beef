04fe7c8fe18b211a66f2ffd7fa60351008841954
jakehsiao <1499975101@qq.com>
2021-04-18 18:37:50 +0800

    Minor changes.

--- src/Product.js
+++ src/Product.js
@@ -3,6 +3,6 @@ import React from 'react'
 export function Product() {
   return <div className="product">
     <div className="product-title">Tastier beef with<br/>less harmful gas.</div>
-    <div className="product-desc">Do you know that eat synthetic beef rather than normal one can save the world?</div>
+    <div className="product-desc">Do you know that eat synthetic beef rather than the normal one can save the world?</div>
   </div>;
 }
